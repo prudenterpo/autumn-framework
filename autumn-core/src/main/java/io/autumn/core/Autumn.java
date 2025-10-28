aa8aa136300343cd1fe8ef814971c22b0e46cff9
rodrigopdo <rodrigo.prudente82@gmail.com>
2025-10-27 22:10:31 -0300

    feat: add .gitignore and pom.xml

---
+++ autumn-core/src/main/java/io/autumn/core/Autumn.java
@@ -0,0 +1,14 @@
+package io.autumn.core;
+
+import io.autumn.core.context.*;
+
+/**
+ * Entry point for bootstrapping the Autumn IoC container.
+ */
+public class Autumn {
+
+    public static AutumnContext start(String basePackage) {
+        // Bootstrapping logic will be added in Phase 3
+        return null;
+    }
+}
